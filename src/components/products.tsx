bf971b7a37154152675c748ab9ca22df89b20f77
Stanislav-3123 <pshedzial.stas@gmail.com>
2023-08-19 22:35:13 +0300

    1

---
+++ src/components/products.tsx
@@ -0,0 +1,29 @@
+import "./products.css";
+import React, { useState } from "react";
+import { IProduct } from "../models";
+
+interface ProductProps {
+  product: IProduct;
+}
+
+export function Product({product}: ProductProps) {
+
+	const [details, setDetails] = useState(false)
+
+  return (
+    <div className="products">
+      <img src={product.image} alt={product.title} className="img-product" />
+      <div className="product-info">
+        <p className="product-title">{product.title}</p>
+        <p className="product-price">{product.price}$</p>
+      </div>
+		<button 
+		onClick={() => setDetails(prev => !prev)}
+		className={details ? "hide-details-button": "show-details-button"}>{details ? 'Hide Details' : 'Show Details' }</button>
+		{/* <button 
+		onClick={() => setDetails(false)}
+		className="hide-details-button">Hide Details</button> */}
+		{details && <p className="product-description">{product.description}</p>}
+    </div>
+  );
+}
